5ab56a9a9cbaf8ae1543de70a176b8fef40eee89
marnepike <Marne@marnes-MacBook-Pro.local>
2015-10-25 00:09:40 -0400

    Final version of week 5 HW
    updated javascript file

--- class10/Assignment/starter_code/js/index.js
+++ class10/Assignment/starter_code/js/index.js
@@ -1,15 +1,3 @@
-// Use the ```$.ready()``` handler to delay your code from executing until all DOM assets have been loaded
-// Select the appropriate DOM elements with CSS selectors upon a user's click using the ```$.click()``` handler
-// If a user clicks "Read More" on the primary column:
-	//have the text in the ```<p>``` tag slide down along with a "Read Less" link in the blog post using  ```$.slideDown()``` and ```$.show()```
-	//hide the "Read More" link using ```$.hide()```
-
-// If a user clicks "Read Less" on the primary column:
-	//have the ```<p>``` slide up and hide the "Read Less" link using  ```$.slideUp()``` and ```$.hide()```
-	//show the "Read More" link using ```$.show()```
-
-// Using the same functions as above, if a user clicks the "Learn More" link in the sidebar, have the ```<span>``` inside that ```<p>``` slide down and hide the "Learn More" link using ```$.slideDown()``` and ```$.hide()```
-
 $(document).ready(function() {
 //1st attempt
 /*function ShowParagraph() {
@@ -32,22 +20,35 @@ $('.readmore').click(function(){
 */
 
 //3rd attempt
-//READMORE BUTTON
+//READMORE BUTTON 1
 $('.readmore').click(function(){ 
     $('#ShowThisOnClick').slideDown();
     $('.readmore').hide();
     $('.readless').show();
-
   });
-//READLESS Button
-('.readless').click(function(){ 
-    $('#ShowThisOnClick').hide();
+//READLESS Button not working
+$('.readless').click(function(){ 
+    $('#ShowThisOnClick').slideUp();
     $('.readmore').show();
-    $('.readless hide').hide();
-
+    $('.readless').hide();
   });
-
-
+//READMORE 2
+$('.readmore2').click(function(){ 
+    $('#ShowThisOnClick2').slideDown();
+    $('.readmore2').hide();
+    $('.readless2').show();
+  });
+//READLESS 2
+$('.readless2').click(function(){ 
+    $('#ShowThisOnClick2').slideUp();
+    $('.readmore2').show();
+    $('.readless2').hide();
+  });
+//Sidebar
+$('.learnmore').click(function(){ 
+    $('#learnmoretext').slideDown();
+    $('.learnmore').hide()
+  });
 
 });
 
